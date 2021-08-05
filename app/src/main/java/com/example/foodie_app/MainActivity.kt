a8edb77d2f8c630beeb294d83cc2064129e91e5a
Celina Rabe <celinarabe@yahoo.com>
2021-08-05 10:42:40 -0500

    got it working

--- app/src/main/java/com/example/foodie_app/MainActivity.kt
+++ app/src/main/java/com/example/foodie_app/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.foodie_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.foodie_app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initialize the bottom navigation view
+        val bottomNavView : BottomNavigationView = binding.bottomNavigation
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
     }
 }
