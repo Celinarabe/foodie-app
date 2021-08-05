e02629e4c336638fb91187510bcd6b06f5d6e18b
Celina Rabe <celinarabe@yahoo.com>
2021-08-05 14:52:45 -0500

    removed boiler plate code from fragments

--- app/src/main/java/com/example/foodie_app/ListFeedFragment.kt
+++ app/src/main/java/com/example/foodie_app/ListFeedFragment.kt
@@ -1,21 +1,13 @@
 package com.example.foodie_app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFeedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class ListFeedFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -23,10 +15,6 @@ class ListFeedFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -37,23 +25,4 @@ class ListFeedFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list_feed, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFeedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFeedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
