a629916cdc90c6cae1c3f67a89957eea5ed00a32
Celina Rabe <celinarabe@yahoo.com>
2021-08-07 15:23:32 -0500

    added photo option

--- app/src/main/java/com/example/foodie_app/NewDishFragment.kt
+++ app/src/main/java/com/example/foodie_app/NewDishFragment.kt
@@ -1,7 +1,9 @@
 package com.example.foodie_app
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,7 @@ import com.example.foodie_app.databinding.FragmentNewDishBinding
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointBackward
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -25,10 +28,11 @@ class NewDishFragment : Fragment() {
     private val today = MaterialDatePicker.todayInUtcMilliseconds()
     private lateinit var datePicker:MaterialDatePicker<Long>
 
+    val REQUEST_IMAGE_CAPTURE = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("NewDishFragment", "$today")
     }
 
     override fun onCreateView(
@@ -46,9 +50,13 @@ class NewDishFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //access date button
         val dateBtn = binding.btnDate
-        dateBtn.text = getDateTime(today.toString())
+        dateBtn.text = getDateTime(today)
         dateBtn.setOnClickListener {
             datePicker.show(this.parentFragmentManager, datePicker.toString())
+        }
+        val addPicBtn = binding.addPicBtn
+        addPicBtn.setOnClickListener {
+            showPictureOptions()
         }
     }
 
@@ -62,18 +70,46 @@ class NewDishFragment : Fragment() {
         datePicker.addOnPositiveButtonClickListener {
             // Respond to positive button click.
             val dateSelection = datePicker.selection
-            val dateString = getDateTime(dateSelection.toString())
+            val dateString = getDateTime(dateSelection!!)
             binding.btnDate.text = dateString
         }
     }
 
-
-
-    private fun getDateTime(s: String): String? {
-        val sdf = SimpleDateFormat("MM/dd/yyyy")
-        val netDate = Date(s.toLong())
-        return sdf.format(netDate)
-
+    private fun dispatchChoosePictureIntent() {
+        val intent1 = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent1, 2)
     }
 
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    private fun showPictureOptions() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Add Photo")
+            .setMessage("Select a method to add a photo")
+            .setCancelable(true)
+            .setNegativeButton("Choose From Gallery") { _, _ ->
+                dispatchChoosePictureIntent()
+            }
+            .setPositiveButton("Take Photo") { _, _ ->
+                dispatchTakePictureIntent()
+            }
+            .show()
+    }
+
+
+    //converts epoch time to date string format
+    private fun getDateTime(s: Long): String? {
+        val timeZone = TimeZone.getDefault()
+        val offset = timeZone.getOffset(s)
+        val sdf = SimpleDateFormat("MM/dd/yyyy")
+        val netDate = Date(s - offset)
+        return sdf.format(netDate)
+    }
 }
