39e579022b84584a0e2b25881e73f765cacbfe83
Celina Rabe <celinarabe@yahoo.com>
2021-08-08 14:45:06 -0500

    added take photo

--- app/src/main/java/com/example/foodie_app/NewDishFragment.kt
+++ app/src/main/java/com/example/foodie_app/NewDishFragment.kt
@@ -1,20 +1,30 @@
 package com.example.foodie_app
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.example.foodie_app.databinding.FragmentNewDishBinding
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointBackward
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.roundToInt
 
 
 class NewDishFragment : Fragment() {
@@ -29,7 +39,7 @@ class NewDishFragment : Fragment() {
     private lateinit var datePicker:MaterialDatePicker<Long>
 
     val REQUEST_IMAGE_CAPTURE = 1
-
+    private lateinit var photoFile : File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +70,15 @@ class NewDishFragment : Fragment() {
         }
     }
 
+    private fun createImageFile() : File {
+        //create file name
+        val timeStamp:String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = this.context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val uniquePrefix:String = "JPEG_${timeStamp}_"
+        return File.createTempFile(uniquePrefix, ".jpg", storageDir)
+
+    }
+
     private fun buildDatePicker() {
         val datePickerBuilder = MaterialDatePicker.Builder.datePicker()
         //constrain calendar to today and backwards
@@ -82,10 +101,39 @@ class NewDishFragment : Fragment() {
 
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        photoFile = createImageFile()
+        //NOTE: this doesnt work for API >= 24
+        //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+        val fileProvider = FileProvider.getUriForFile(this.requireContext(), "com.example.foodie_app.fileprovider", photoFile)
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileProvider) //passing in a file provider allows for more secure content sharing
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         } catch (e: ActivityNotFoundException) {
             // display error state to the user
+            Toast.makeText(this.context, "Unable to open camera", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+//            val takenImage = data?.extras?.get("data") as Bitmap //low quality image
+            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            Log.d("NewDishFragment", "${photoFile.absolutePath}")
+            Log.d("NewDishFragment", "${takenImage}")
+            val displayMetrics = DisplayMetrics()
+            activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
+
+            val aspectRatio = takenImage.width / takenImage.height
+            val newWidth = displayMetrics.widthPixels
+
+            val newHeight = (newWidth.toDouble() / aspectRatio).roundToInt()
+
+
+
+            Log.d("NewDishFragment", "${displayMetrics.widthPixels}, ${displayMetrics.heightPixels}")
+            binding.imageView.setImageBitmap(Bitmap.createScaledBitmap(takenImage, newWidth, newHeight, false))
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
